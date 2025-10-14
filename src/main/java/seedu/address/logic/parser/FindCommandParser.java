4dfa7c27f314d65e20057573aea3bbe3eb0ce0b9
Albertd13 <albert.ding.13@gmail.com>
2025-10-14 12:41:42 +0800

    update find command to match multiple attributes

--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -1,33 +1,78 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.*;
 
 import java.util.Arrays;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.person.NameContainsKeywordsPredicate;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.StrAttrContainsKeywords;
 
 /**
  * Parses input arguments and creates a new FindCommand object
  */
 public class FindCommandParser implements Parser<FindCommand> {
 
+    private static final Predicate<Person> PREDICATE_FALSE = person -> false;
     /**
      * Parses the given {@code String} of arguments in the context of the FindCommand
      * and returns a FindCommand object for execution.
+     *
      * @throws ParseException if the user input does not conform the expected format
      */
     public FindCommand parse(String args) throws ParseException {
-        String trimmedArgs = args.trim();
-        if (trimmedArgs.isEmpty()) {
-            throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_TAG);
+        if (!atLeastOnePrefixPresent(argMultimap, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_TAG)
+                || !argMultimap.getPreamble().isEmpty() ) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+        }
+        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_TAG);
+
+        // build predicate for each attribute
+        Predicate<Person> namePredicate = argMultimap.getValue(PREFIX_NAME)
+                .map( keywordStr -> buildPredicate(keywordStr, person -> person.getName().fullName))
+                .orElse(PREDICATE_FALSE);
+        Predicate<Person> phonePredicate = argMultimap.getValue(PREFIX_PHONE)
+                .map( keywordStr -> buildPredicate(keywordStr, person -> person.getPhone().value))
+                .orElse(PREDICATE_FALSE);
+        Predicate<Person> emailPredicate = argMultimap.getValue(PREFIX_EMAIL)
+                .map( keywordStr -> buildPredicate(keywordStr, person -> person.getEmail().value))
+                .orElse(PREDICATE_FALSE);
+        Predicate<Person> addressPredicate = argMultimap.getValue(PREFIX_ADDRESS)
+                .map( keywordStr -> buildPredicate(keywordStr, person -> person.getAddress().value))
+                .orElse(PREDICATE_FALSE);
+        // TODO: use a different predicate for tags since it's a set of Tag objects
+        Predicate<Person> tagPredicate = argMultimap.getValue(PREFIX_TAG)
+                .map( keywordStr -> buildPredicate(keywordStr, person -> person.getTags().toString()))
+                .orElse(PREDICATE_FALSE);
+
+        if (namePredicate == PREDICATE_FALSE && phonePredicate == PREDICATE_FALSE
+                && emailPredicate == PREDICATE_FALSE && addressPredicate == PREDICATE_FALSE
+                && tagPredicate == PREDICATE_FALSE) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
         }
 
-        String[] nameKeywords = trimmedArgs.split("\\s+");
-
-        return new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+        return new FindCommand(namePredicate.or(phonePredicate)
+                .or(emailPredicate)
+                .or(addressPredicate)
+                .or(tagPredicate));
     }
 
+    /**
+     * Builds predicate based on a string of keywords and a Person attribute getter
+     * @param keywordString containing keywords to search for
+     * @return predicate that evaluates to true if at least one of the keywords is found in the attribute
+     */
+    private static Predicate<Person> buildPredicate(String keywordString, Function<Person, String> attributeGetter) {
+        String[] nameKeywords = keywordString.trim().split("\\s+");
+        return new StrAttrContainsKeywords(Arrays.asList(nameKeywords), attributeGetter);
+    }
+    private static boolean atLeastOnePrefixPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Arrays.stream(prefixes).anyMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
+    }
 }
--- src/main/java/seedu/address/model/person/StrAttrContainsKeywords.java
+++ src/main/java/seedu/address/model/person/StrAttrContainsKeywords.java
@@ -1,6 +1,7 @@
 package seedu.address.model.person;
 
 import java.util.List;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 import seedu.address.commons.util.StringUtil;
@@ -9,17 +10,19 @@ import seedu.address.commons.util.ToStringBuilder;
 /**
  * Tests that a {@code Person}'s {@code Name} matches any of the keywords given.
  */
-public class NameContainsKeywordsPredicate implements Predicate<Person> {
+public class StrAttrContainsKeywords implements Predicate<Person> {
     private final List<String> keywords;
+    private final Function<Person, String> attributeGetter;
 
-    public NameContainsKeywordsPredicate(List<String> keywords) {
+    public StrAttrContainsKeywords(List<String> keywords, Function<Person, String> attributeGetter) {
         this.keywords = keywords;
+        this.attributeGetter = attributeGetter;
     }
 
     @Override
     public boolean test(Person person) {
         return keywords.stream()
-                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getName().fullName, keyword));
+                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(attributeGetter.apply(person), keyword));
     }
 
     @Override
@@ -29,12 +32,12 @@ public class NameContainsKeywordsPredicate implements Predicate<Person> {
         }
 
         // instanceof handles nulls
-        if (!(other instanceof NameContainsKeywordsPredicate)) {
+        if (!(other instanceof StrAttrContainsKeywords)) {
             return false;
         }
 
-        NameContainsKeywordsPredicate otherNameContainsKeywordsPredicate = (NameContainsKeywordsPredicate) other;
-        return keywords.equals(otherNameContainsKeywordsPredicate.keywords);
+        StrAttrContainsKeywords otherStrAttrContainsKeywords = (StrAttrContainsKeywords) other;
+        return keywords.equals(otherStrAttrContainsKeywords.keywords);
     }
 
     @Override
