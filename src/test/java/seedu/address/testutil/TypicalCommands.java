2fc531f98f8d845c3033498de673e67f3a845062
ruissouw <rohit16@live.com>
2025-10-15 23:33:29 +0800

    create mock commands for testing

--- src/test/java/seedu/address/testutil/TypicalCommands.java
+++ src/test/java/seedu/address/testutil/TypicalCommands.java
@@ -1,4 +1,12 @@
 package seedu.address.testutil;
 
+import java.util.List;
+
 public class TypicalCommands {
+    public static final String ADD_PERSON_COMMAND = "add n/John Doe p/98765432 e/johnd@example.com a/311, " +
+            "Clementi Ave 2, #02-25 t/volunteer t/owesMoney";
+    public static final String DELETE_PERSON_COMMAND = "delete 1";
+    public static final String LIST_COMMAND = "list";
+    public static final List<String> LIST_OF_COMMANDS = List.of(ADD_PERSON_COMMAND,
+            DELETE_PERSON_COMMAND, LIST_COMMAND);
 }
