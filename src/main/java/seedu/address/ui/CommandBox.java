b328a329ac0ce17c04db4719a5001aecc2e8d1a0
ruissouw <rohit16@live.com>
2025-10-16 00:09:35 +0800

    fix import statements

--- src/main/java/seedu/address/ui/CommandBox.java
+++ src/main/java/seedu/address/ui/CommandBox.java
@@ -1,5 +1,7 @@
 package seedu.address.ui;
 
+import java.util.function.Supplier;
+
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
@@ -9,8 +11,6 @@ import javafx.scene.layout.Region;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.exceptions.ParseException;
-
-import java.util.function.Supplier;
 
 /**
  * The UI component that is responsible for receiving user command inputs.
