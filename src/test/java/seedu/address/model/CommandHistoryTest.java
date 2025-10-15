c32d1efc73883858a0d45ffdb6d1b7fb9ba671ee
ruissouw <rohit16@live.com>
2025-10-15 23:33:09 +0800

    create command history test cases

--- src/test/java/seedu/address/model/CommandHistoryTest.java
+++ src/test/java/seedu/address/model/CommandHistoryTest.java
@@ -1,4 +1,102 @@
 package seedu.address.model;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.TypicalCommands.ADD_PERSON_COMMAND;
+import static seedu.address.testutil.TypicalCommands.DELETE_PERSON_COMMAND;
+import static seedu.address.testutil.TypicalCommands.LIST_COMMAND;
+import static seedu.address.testutil.TypicalCommands.LIST_OF_COMMANDS;
+
+import org.junit.jupiter.api.Test;
+
 public class CommandHistoryTest {
+
+    private final CommandHistory commandHistory = new CommandHistory();
+
+    @Test
+    public void constructor() {
+        assertFalse(commandHistory.hasCommands());
+        assertEquals("", commandHistory.getPreviousCommand());
+        assertEquals("", commandHistory.getNextCommand());
+    }
+
+    @Test
+    public void addCommand_singleCommand_updatesCommandHistory() {
+        commandHistory.addCommand(ADD_PERSON_COMMAND);
+        // check command has been added
+        assertTrue(commandHistory.hasCommands());
+
+        // should stay on same command
+        assertEquals(ADD_PERSON_COMMAND, commandHistory.getPreviousCommand());
+        assertEquals(ADD_PERSON_COMMAND, commandHistory.getNextCommand());
+    }
+
+    @Test
+    public void addCommand_multipleCommands_updatesCommandHistory() {
+        for (String commandText : LIST_OF_COMMANDS) {
+            commandHistory.addCommand(commandText);
+        }
+        // check commands have been added
+        assertTrue(commandHistory.hasCommands());
+    }
+
+    @Test
+    public void hasPrevious_noCommandHistory_returnFalse() {
+        assertFalse(commandHistory.hasPrevious());
+    }
+
+    @Test
+    public void hasPrevious_hasCommandHistory_returnTrue() {
+        for (String commandText : LIST_OF_COMMANDS) {
+            commandHistory.addCommand(commandText);
+        }
+        assertTrue(commandHistory.hasPrevious());
+    }
+
+    @Test
+    public void hasNext_noCommandHistory_returnFalse() {
+        assertFalse(commandHistory.hasNext());
+    }
+
+    @Test
+    public void hasNext_hasCommandHistory_returnTrue() {
+        for (String commandText : LIST_OF_COMMANDS) {
+            commandHistory.addCommand(commandText);
+        }
+        String previousCommand = commandHistory.getPreviousCommand();
+        assertTrue(commandHistory.hasNext());
+    }
+
+    @Test
+    public void getPreviousCommand_singleCommand_returnCurrentCommand() {
+        commandHistory.addCommand(ADD_PERSON_COMMAND);
+        assertEquals(ADD_PERSON_COMMAND, commandHistory.getPreviousCommand());
+    }
+
+    @Test
+    public void getPreviousCommand_multipleCommands_returnCorrespondingCommand() {
+        for (String commandText : LIST_OF_COMMANDS) {
+            commandHistory.addCommand(commandText);
+        }
+        assertEquals(DELETE_PERSON_COMMAND, commandHistory.getPreviousCommand());
+        assertEquals(ADD_PERSON_COMMAND, commandHistory.getPreviousCommand());
+    }
+
+    @Test
+    public void getNextCommand_singleCommand_returnCurrentCommand() {
+        commandHistory.addCommand(ADD_PERSON_COMMAND);
+        assertEquals(ADD_PERSON_COMMAND, commandHistory.getNextCommand());
+    }
+
+    @Test
+    public void getNextCommand_multipleCommands_returnCorrespondingCommand() {
+        for (String commandText : LIST_OF_COMMANDS) {
+            commandHistory.addCommand(commandText);
+        }
+        assertEquals(DELETE_PERSON_COMMAND, commandHistory.getPreviousCommand());
+        assertEquals(ADD_PERSON_COMMAND, commandHistory.getPreviousCommand());
+        assertEquals(DELETE_PERSON_COMMAND, commandHistory.getNextCommand());
+        assertEquals(LIST_COMMAND, commandHistory.getNextCommand());
+    }
 }
