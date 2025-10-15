1bfa9a5fe691af891071e39989d2a68f1f85583f
ruissouw <rohit16@live.com>
2025-10-15 23:54:19 +0800

    fix checkstyle errors

--- src/test/java/seedu/address/testutil/TypicalCommands.java
+++ src/test/java/seedu/address/testutil/TypicalCommands.java
@@ -2,9 +2,12 @@ package seedu.address.testutil;
 
 import java.util.List;
 
+/**
+ * A utility class containing a list of {@code String} commands to be used in tests.
+ */
 public class TypicalCommands {
-    public static final String ADD_PERSON_COMMAND = "add n/John Doe p/98765432 e/johnd@example.com a/311, " +
-            "Clementi Ave 2, #02-25 t/volunteer t/owesMoney";
+    public static final String ADD_PERSON_COMMAND = "add n/John Doe p/98765432 e/johnd@example.com a/311, "
+            + "Clementi Ave 2, #02-25 t/volunteer t/owesMoney";
     public static final String DELETE_PERSON_COMMAND = "delete 1";
     public static final String LIST_COMMAND = "list";
     public static final List<String> LIST_OF_COMMANDS = List.of(ADD_PERSON_COMMAND,
