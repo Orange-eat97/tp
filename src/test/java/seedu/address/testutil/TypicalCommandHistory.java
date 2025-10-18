0435d8a7d5e98d9bdb4dddbb9294a5b7146f097b
ruissouw <rohit16@live.com>
2025-10-18 19:24:57 +0800

    Create mock data for command history

--- src/test/java/seedu/address/testutil/TypicalCommandHistory.java
+++ src/test/java/seedu/address/testutil/TypicalCommandHistory.java
@@ -1,4 +1,16 @@
 package seedu.address.testutil;
 
+import seedu.address.commons.util.StringUtil;
+
+import java.util.List;
+
 public class TypicalCommandHistory {
+
+    private static final String COMMAND_HISTORY_HEADING = "Command History:\n";
+
+    public static String ONE_COMMAND_COMMAND_HISTORY = COMMAND_HISTORY_HEADING + StringUtil.formatNumberedListWithHighlight(
+            List.of(TypicalCommands.ADD_PERSON_COMMAND), 0);
+    public static String THREE_COMMANDS_COMMAND_HISTORY = COMMAND_HISTORY_HEADING + StringUtil.formatNumberedListWithHighlight(
+            TypicalCommands.LIST_OF_COMMANDS, 2);
+
 }
