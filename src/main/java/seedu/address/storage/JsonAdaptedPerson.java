70c46bcae51da2b6b2c1968c917e6a8429cccd0e
Chrogain <aidantan128@gmail.com>
2025-11-02 18:16:19 +0800

    Fix checkstyle error

--- src/main/java/seedu/address/storage/JsonAdaptedPerson.java
+++ src/main/java/seedu/address/storage/JsonAdaptedPerson.java
@@ -12,7 +12,6 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import seedu.address.commons.exceptions.IllegalValueException;
-import seedu.address.model.AddressBook;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
@@ -72,8 +71,8 @@ class JsonAdaptedPerson {
      * @throws IllegalValueException if there were any data constraints violated in the adapted person.
      */
     public Person toModelType() throws IllegalValueException {
-        if (tags.contains(new JsonAdaptedTag("volunteer")) &&
-                tags.contains(new JsonAdaptedTag("beneficiary"))) {
+        if (tags.contains(new JsonAdaptedTag("volunteer"))
+                && tags.contains(new JsonAdaptedTag("beneficiary"))) {
             throw new IllegalValueException(Tag.MESSAGE_CONSTRAINTS);
         }
         final List<Tag> personTags = new ArrayList<>();
